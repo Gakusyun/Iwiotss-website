04dd8f87122c6782c4ecd8e6cfd71df01f7d6e42
Gakusyun <gaoxj040620@outlook.com>
2024-06-23 21:41:45 +0800

    tsbu

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <a href="">首页</a>
+    <a href="">关于</a>
+</template>
+
+<style scoped>
+a {
+    height: 50px;
+    width: 70px;
+    line-height: 50px;
+    text-align: center;
+    float: left;
+    display: block;
+}
+</style>
